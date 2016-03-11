18c647d57b0c5b2493d32567e5b18316bb123eea
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2016-03-11 20:07:37 +0200

    Added action_types table definition and seed data

--- migrations/20160311171339_initial.js
+++ migrations/20160311171339_initial.js
@@ -6,18 +6,34 @@ exports.up = function(knex, Promise) {
     table.timestamp('updated_at').index().notNullable().defaultTo(knex.fn.now());
   })
   .then(() => {
+    return knex.schema.createTable('action_types', function(table) {
+      table.bigIncrements('id').primary().index();
+      table.string('name').notNullable().unique();
+      table.integer('value').unsigned().notNullable();
+      table.integer('cooldown').unsigned().notNullable(); // ms
+
+      table.timestamp('created_at').index().notNullable().defaultTo(knex.fn.now());
+      table.timestamp('updated_at').index().notNullable().defaultTo(knex.fn.now());
+    });
+  })
+  .then(() => {
     return knex.schema.createTable('actions', function(table) {
       table.bigIncrements('id').primary().index();
       table.string('user_uuid').notNullable().index();
       table.integer('team_id').unsigned().notNullable();
       table.foreign('team_id')
-          .references('id')
-          .inTable('teams')
-          .onDelete('RESTRICT')
-          .onUpdate('CASCADE');
+        .references('id')
+        .inTable('teams')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
 
       table.specificType('location', 'point').notNullable().index('index_location', 'GIST');
-      table.string('type').notNullable().index();
+      table.integer('type_id').unsigned().notNullable();
+      table.foreign('type_id')
+        .references('id')
+        .inTable('action_types')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
       table.json('payload');
 
       table.timestamp('created_at').index().notNullable().defaultTo(knex.fn.now());
@@ -28,6 +44,9 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTable('actions')
+  .then(() => {
+    return knex.schema.dropTable('action_types');
+  })
   .then(() => {
     return knex.schema.dropTable('teams');
   });
