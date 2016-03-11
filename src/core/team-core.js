80d53b69fd38e7e9e156798862d0b130d0cdaadc
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2016-03-11 20:07:57 +0200

    teams endpoint returns now teams in order of their score

--- src/core/team-core.js
+++ src/core/team-core.js
@@ -3,9 +3,15 @@ import _ from 'lodash';
 import {deepChangeKeyCase} from '../util';
 
 function getTeams() {
-  return knex('teams').select()
-  .then(rows => {
-    return _.map(rows, row => deepChangeKeyCase(row, 'camelCase'));
+  return knex.raw(`SELECT teams.id, teams.name, SUM(COALESCE(action_types.value, 0)) AS score
+  FROM teams
+  LEFT JOIN actions ON teams.id = actions.team_id
+  LEFT JOIN action_types ON actions.type_id = action_types.id
+  GROUP BY teams.id, teams.name
+  ORDER BY score DESC, teams.id`)
+  .then(result => {
+      console.log(result.rows);
+    return _.map(result.rows, row => deepChangeKeyCase(row, 'camelCase'));
   });
 }
 
