d914cf39e0fa815c97138a9bd5b38b682ee73fb7
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2016-03-11 19:04:18 +0200

    Removed unused import

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import * as eventHttp from './http/event-http';
-import * as leaderboardHttp from './http/leaderboard-http';
 import * as actionHttp from './http/action-http';
 import * as teamHttp from './http/team-http';
 
