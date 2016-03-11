770ceff82bdb04bd6f12feadc6d783e788c318d2
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-03-11 22:55:58 +0200

    Make seeds more verbose

--- src/util/seeds.js
+++ src/util/seeds.js
@@ -3,8 +3,10 @@ function insertOrUpdate(knex, table, row) {
   return knex(table).select().where('id', row.id)
     .then(rows => {
       if (rows.length > 0) {
+        console.log('Update row id', row.id, 'in', table);
         return knex(table).where('id', row.id).update(row);
       } else {
+        console.log('Insert row with id', row.id, 'to', table);
         return knex(table).insert(row);
       }
     });
