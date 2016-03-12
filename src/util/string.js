d212d53bb6ca7fe78fcbef38ed201adf294b8ed0
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2016-03-12 15:31:51 +0200

    Added string util

---
+++ src/util/string.js
@@ -0,0 +1,16 @@
+/**
+ * Pads given value into given length with zeros
+ */
+function padLeft(toPad, size) {
+    let s = toPad+"";
+
+    while (s.length < size) {
+			s = "0" + s;
+		}
+
+    return s;
+}
+
+export {
+	padLeft
+};
