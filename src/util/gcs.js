7ad20d1862f9ec4368fcde7fbe1c4a24ad2012bb
Tomi Turtiainen <tomi.turtiainen@futurice.com>
2016-03-12 20:04:29 +0200

    Added requireEnvs to gcs

--- src/util/gcs.js
+++ src/util/gcs.js
@@ -2,28 +2,44 @@
 
 import streamifier from 'streamifier';
 import * as base64 from './base64';
+const requireEnvs = require('./require-envs');
+requireEnvs([
+  'GCS_PRIVATE_KEY',
+  'GCS_BUCKET_NAME',
+  'GCS_TYPE',
+  'GCS_PROJECT_ID',
+  'GCS_PRIVATE_KEY_ID',
+  'GCS_PRIVATE_KEY',
+  'GCS_CLIENT_EMAIL',
+  'GCS_CLIENT_ID',
+  'GCS_AUTH_URI',
+  'GCS_TOKEN_URI',
+  'GCS_AUTH_PROVIDER_X509_CERT_URL',
+  'GCS_CLIENT_X509_CERT_URL',
+  'GCS_CLIENT_EMAIL'
+]);
 
 const PRIVATE_KEY = base64.decodeBase64String(process.env.GCS_PRIVATE_KEY);
 const GCS_CONFIG = {
-    bucketName: process.env.GCS_BUCKET_NAME,
-    baseUrl: 'https://storage.googleapis.com',
+  bucketName: process.env.GCS_BUCKET_NAME,
+  baseUrl: 'https://storage.googleapis.com',
 
-    "type": process.env.GCS_TYPE,
-    "projectId": process.env.GCS_PROJECT_ID,
-    "project_id": process.env.GCS_PROJECT_ID,
-    "private_key_id": process.env.GCS_PRIVATE_KEY_ID,
-    "private_key": process.env.GCS_PRIVATE_KEY,
-    "client_email": process.env.GCS_CLIENT_EMAIL,
-    "client_id": process.env.GCS_CLIENT_ID,
-    "auth_uri": process.env.GCS_AUTH_URI,
-    "token_uri": process.env.GCS_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.GCS_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.GCS_CLIENT_X509_CERT_URL,
+  "type": process.env.GCS_TYPE,
+  "projectId": process.env.GCS_PROJECT_ID,
+  "project_id": process.env.GCS_PROJECT_ID,
+  "private_key_id": process.env.GCS_PRIVATE_KEY_ID,
+  "private_key": process.env.GCS_PRIVATE_KEY,
+  "client_email": process.env.GCS_CLIENT_EMAIL,
+  "client_id": process.env.GCS_CLIENT_ID,
+  "auth_uri": process.env.GCS_AUTH_URI,
+  "token_uri": process.env.GCS_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.GCS_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.GCS_CLIENT_X509_CERT_URL,
 
-    credentials: {
-        "private_key": PRIVATE_KEY,
-        "client_email": process.env.GCS_CLIENT_EMAIL
-    }
+  credentials: {
+    "private_key": PRIVATE_KEY,
+    "client_email": process.env.GCS_CLIENT_EMAIL
+  }
 };
 const gcloud = require('gcloud')(GCS_CONFIG);
 
@@ -34,7 +50,7 @@ const bucket = gcs.bucket(process.env.GCS_BUCKET_NAME);
 //
 // uploadImage
 function uploadImageBuffer(imageName, imageBuffer) {
-    console.log("Uploading");
+  console.log("Uploading");
   return new Promise(function(resolve, reject) {
     const file = bucket.file(imageName);
 
@@ -60,5 +76,5 @@ function uploadImageBuffer(imageName, imageBuffer) {
 };
 
 export {
-  uploadImageBuffer
+uploadImageBuffer
 };
