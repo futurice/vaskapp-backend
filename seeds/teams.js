967458becb6f233bb1318907646fed438eb64a5d
Miro Nieminen <miro.nieminen@futurice.com>
2016-03-16 16:55:39 +0200

    Add team images to seed file

--- seeds/teams.js
+++ seeds/teams.js
@@ -3,72 +3,84 @@ var util = require('../src/util/seeds');
 exports.seed = function(knex, Promise) {
   return util.insertOrUpdate(knex, 'teams', {
     id: 1,
-    name: 'TiTe'
+    name: 'TiTe',
+    image_path: 'https://storage.googleapis.com/wappuapp/assets/tite.png'
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 2,
-      name: 'Skilta'
+      name: 'Skilta',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/skilta.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 3,
-      name: 'Autek'
+      name: 'Autek',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/autek.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 4,
-      name: 'Bioner'
+      name: 'Bioner',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/bioner.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 5,
-      name: 'Hiukkanen'
+      name: 'Hiukkanen',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/hiukkanen.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 6,
-      name: 'Indecs'
+      name: 'Indecs',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/indecs.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 7,
-      name: 'Koneenrakentajakilta'
+      name: 'KoRK',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/kork.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 8,
-      name: 'Man@ger'
+      name: 'Man@ger',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/manager.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 9,
-      name: 'Materiaali-insinöörikilta'
+      name: 'Materiaali-insinöörikilta',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/mik.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 10,
-      name: 'TamArk'
+      name: 'TamArk',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/tamark.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 11,
-      name: 'TARAKI'
+      name: 'TARAKI',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/taraki.png'
     });
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 12,
-      name: 'YKI'
+      name: 'YKI',
+      image_path: 'https://storage.googleapis.com/wappuapp/assets/yki.png'
     });
   });
 };
