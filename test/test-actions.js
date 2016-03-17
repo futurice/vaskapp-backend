6be0e428cea32eb64a5653e19be3cb8b39eb18c4
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-03-17 20:19:40 +0200

    Added test for /api/actions TEXT action

--- test/test-actions.js
+++ test/test-actions.js
@@ -1,7 +1,7 @@
 const request = require('./util/request')();
 
 function testActions() {
-  describe('/api/actions', function() {
+  describe('/api/actions', function () {
     it('create beer action', () => {
       return request
         .post('/api/actions')
@@ -13,6 +13,21 @@ function testActions() {
             latitude: 0,
             longitude: 0
           }
+        })
+        .expect(200);
+    });
+
+    it('create text action', () => {
+      return request
+        .post('/api/actions')
+        .send({
+          'type': 'TEXT',
+          'user': 'hessu',
+          'location': {
+            'latitude': '0.123',
+            'longitude': '0.123'
+          },
+          'text': 'LOLOLOLOLO'
         })
         .expect(200);
     });
