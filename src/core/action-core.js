8be8305b217e83ad4ef1f8e64ec885d378b02828
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-03-17 20:16:57 +0200

    Create feed item on IMAGE and TEXT actions

--- src/core/action-core.js
+++ src/core/action-core.js
@@ -1,28 +1,35 @@
 import _ from 'lodash';
 const {knex} = require('../util/database').connect();
+import {createFeedItem} from './feed-core';
 
 function createAction(action) {
-  const dbRow = {
-    'team_id': knex.raw('(SELECT team_id from users WHERE uuid = ?)', [action.user]),
+  const actionRow = {
+    'team_id':        knex.raw('(SELECT team_id from users WHERE uuid = ?)', [action.user]),
     'action_type_id': knex.raw('(SELECT id from action_types WHERE code = ?)', [action.type]),
-    'user_id': knex.raw('(SELECT id from users WHERE uuid = ?)', [action.user]),
+    'user_id':        knex.raw('(SELECT id from users WHERE uuid = ?)', [action.user]),
     // Tuple is in longitude, latitude format in Postgis
-    location: action.location.longitude + ',' + action.location.longitude,
-    'image_path': action.imagePath
+    'location':       action.location.longitude + ',' + action.location.longitude,
+    'image_path':     action.imagePath,
+    'text':           action.text
   };
 
-  if (action.text) {
-    dbRow.text = action.text;
-  }
+  // These are inserted directly into feed as well
+  const feedActions = new Set(['IMAGE', 'TEXT']);
 
-  return knex('actions').returning('*').insert(dbRow)
-    .then(rows => {
-      if (_.isEmpty(rows)) {
-        throw new Error('Action row creation failed: ' + dbRow);
-      }
+  return knex.transaction(function(trx) {
+    return trx('actions').returning('*').insert(actionRow)
+      .then(rows => {
+        if (_.isEmpty(rows)) {
+          throw new Error('Action row creation failed: ' + actionRow);
+        }
 
-      return rows.length;
-    });
+        if (feedActions.has(action.type)) {
+          return createFeedItem(action, trx);
+        } else {
+          return rows.length;
+        }
+      });
+  });
 }
 
 function getActionType(code) {
