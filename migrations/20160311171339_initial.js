46f108e76bfe36e9248ae8b755aa6bdd51989343
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-03-17 18:55:19 +0200

    Added schema for feed_items table

--- migrations/20160311171339_initial.js
+++ migrations/20160311171339_initial.js
@@ -37,7 +37,7 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('actions', function(table) {
       table.bigIncrements('id').primary().index();
 
-      table.integer('user_id').unsigned().notNullable().index();
+      table.integer('user_id').unsigned().index();
       table.foreign('user_id')
         .references('id')
         .inTable('users')
@@ -51,13 +51,34 @@ exports.up = function(knex, Promise) {
         .onDelete('RESTRICT')
         .onUpdate('CASCADE');
 
-      table.specificType('location', 'point').notNullable().index('index_location', 'GIST');
+      table.specificType('location', 'point').index('actions_index_location', 'GIST');
       table.integer('action_type_id').unsigned().notNullable();
       table.foreign('action_type_id')
         .references('id')
         .inTable('action_types')
         .onDelete('RESTRICT')
         .onUpdate('CASCADE');
+
+      table.string('image_path');
+      table.string('text', 151);
+
+      table.timestamp('created_at').index().notNullable().defaultTo(knex.fn.now());
+      table.timestamp('updated_at').index().notNullable().defaultTo(knex.fn.now());
+    });
+  })
+  .then(() => {
+    return knex.schema.createTable('feed_items', function(table) {
+      table.bigIncrements('id').primary().index();
+
+      // Null for 'system' user
+      table.integer('user_id').unsigned().index();
+      table.foreign('user_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+
+      table.specificType('location', 'point').notNullable().index('feed_items_index_location', 'GIST');
 
       table.string('image_path');
       table.string('text', 151);
@@ -78,5 +99,8 @@ exports.down = function(knex, Promise) {
   })
   .then(() => {
     return knex.schema.dropTable('teams');
+  })
+  .then(() => {
+    return knex.schema.dropTable('feed');
   });
 };
