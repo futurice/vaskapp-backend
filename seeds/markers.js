d615e89d1dbab76939b3b5f3789fd9be3f54bfa2
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2016-03-22 00:49:48 +0200

    Add markers endpoint

---
+++ seeds/markers.js
@@ -0,0 +1,20 @@
+const BPromise = require('bluebird');
+const markers = require('../data/markers.json');
+var util = require('../src/util/seeds');
+
+exports.seed = function(knex, Promise) {
+  return BPromise.map(markers, (marker, index) => {
+    const row = {
+      id: index + 1,
+      type: marker.type,
+      location: marker.location.longitude + ',' + marker.location.latitude,
+      title: marker.title,
+    };
+
+    if (marker.url) {
+      row.url = marker.url;
+    }
+
+    return util.insertOrUpdate(knex, 'markers', row);
+  }, {concurrency: 1})
+};
---
+++ src/core/marker-core.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+const {knex} = require('../util/database').connect();
+
+function getMarkers() {
+  return knex('markers')
+    .select('*')
+    .then(rows => {
+      if (_.isEmpty(rows)) {
+        return [];
+      }
+
+      return _.map(rows, _markerRowToObject);
+    });
+}
+
+function _markerRowToObject(row) {
+  let obj = {
+    id: row.id,
+    // Postgis uses x(lng), y(lat)
+    location: {
+      latitude: row.location.y,
+      longitude: row.location.x
+    },
+    type: row.type,
+    title: row.title,
+  };
+
+  if (row.url) {
+    obj.url = row.url
+  }
+
+  return obj;
+}
+
+export {
+  getMarkers
+};
