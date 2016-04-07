7a9a992ed7ea73f67a31944035dc8e7a5779d3f4
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-04-07 22:49:08 +0300

    Check if user is banned in /actions API

--- src/http/action-http.js
+++ src/http/action-http.js
@@ -1,6 +1,7 @@
 import * as actionCore from '../core/action-core';
 import {createJsonRoute, throwStatus} from '../util/express';
 import {assert} from '../validation';
+import * as banCore from '../core/ban-core';
 import * as imageHttp from './image-http';
 import * as throttleCore from '../core/throttle-core';
 
@@ -9,6 +10,8 @@ let postAction = createJsonRoute(function(req, res) {
 
   if (!throttleCore.canDoAction(action.user, action.type)) {
     throwStatus(429, `Too many actions of type ${ action.type }`);
+  } else if (banCore.isUserBanned(action.user)) {
+    banCore.throwBannedError();
   }
 
   let handleAction;
