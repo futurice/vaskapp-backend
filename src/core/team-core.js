9b45e88900221e8454f86d4132be00f81235cae4
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-04-08 20:21:37 +0300

    Count banned action scores only for banned users

--- src/core/team-core.js
+++ src/core/team-core.js
@@ -2,15 +2,19 @@ const {knex} = require('../util/database').connect();
 import _ from 'lodash';
 import {deepChangeKeyCase} from '../util';
 
-function getTeams() {
-  return knex.raw(`
+function getTeams(client) {
+  const isBanned = client && !!client.isBanned;
+
+  let sqlString = `
   SELECT teams.id, teams.name, teams.image_path,
     SUM(COALESCE(action_types.value, 0)) AS score
   FROM teams
-  LEFT JOIN actions ON teams.id = actions.team_id AND NOT actions.is_banned
+  LEFT JOIN actions ON teams.id = actions.team_id ${isBanned ? '' : 'AND NOT actions.is_banned'}
   LEFT JOIN action_types ON actions.action_type_id = action_types.id
   GROUP BY teams.id, teams.name
-  ORDER BY score DESC, teams.id`)
+  ORDER BY score DESC, teams.id`;
+
+  return knex.raw(sqlString)
   .then(result => {
     return _.map(result.rows, row => deepChangeKeyCase(row, 'camelCase'));
   });
--- src/http/team-http.js
+++ src/http/team-http.js
@@ -2,7 +2,7 @@ import * as teamCore from '../core/team-core';
 import {createJsonRoute} from '../util/express';
 
 let getTeams = createJsonRoute(function(req, res) {
-  return teamCore.getTeams();
+  return teamCore.getTeams(req.client);
 });
 
 export {
