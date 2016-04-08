8af1bc205811d2b7c92bbedbdb0368b43305ed00
Tomi Turtiainen <tomi.turtiainen@gmail.com>
2016-04-08 19:22:54 +0300

    Query user in require-client-headers middleware

--- src/middleware/require-client-headers.js
+++ src/middleware/require-client-headers.js
@@ -1,3 +1,5 @@
+import * as userCore from '../core/user-core';
+
 function createRequireClientHeaders(opts) {
   return function requireClientHeaders(req, res, next) {
     // Enforce uuid header in other methods than GET
@@ -7,13 +9,25 @@ function createRequireClientHeaders(opts) {
       return next(err);
     }
 
-    req.client = {
-      // If header is not set, set it to "anonymous". It is better then null
-      // because NULL value of uuid in database means system user.
-      uuid: req.headers['x-user-uuid'] || 'anonymous'
-    };
+    const uuid = req.headers['x-user-uuid'] || 'anonymous';
 
-    next();
+    userCore.findByUuid(uuid)
+      .then(user => {
+        if (user) {
+          req.client = user;
+        } else {
+          // If header is not set, set it to "anonymous". It is better then null
+          // because NULL value of uuid in database means system user.
+          req.client = {
+            id: null,
+            isBanned: false,
+            uuid: 'anonymous'
+          };
+        }
+
+        console.log(req.client);
+        next();
+      });
   };
 }
 
