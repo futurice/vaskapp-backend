7bdfabd7adec52da37dbfaebe6e273987e7857a6
Tomy Salminen <tomy.salminen@futurice.com>
2017-02-27 19:09:21 +0200

    Disabled voting on feed type 'SYSTEM'

--- src/core/vote-core.js
+++ src/core/vote-core.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 const {knex} = require('../util/database').connect();
 
 function createOrUpdateVote(vote) {
-  // TODO: Check that feed_item_id is image and not system
 
   const updateSql = `
     WITH upsert AS
@@ -10,12 +9,23 @@ function createOrUpdateVote(vote) {
       INSERT INTO votes (value, user_id, feed_item_id)
       SELECT ?, ?, ? WHERE NOT EXISTS (SELECT * FROM upsert)
   `;
+
   const updateParams = [vote.value, vote.user_id, vote.feed_item_id,
     vote.value, vote.user_id, vote.feed_item_id];
 
   return knex.transaction(function(trx) {
-    return knex.raw(updateSql, updateParams)
-      .transacting(trx)
+    return knex.select()
+      .from('feed_items')
+      .where({ id: vote.feed_item_id })
+      .first()
+      .then(row => {
+        if (!!row && row.type === 'SYSTEM') Promise.reject();
+        return knex.raw(updateSql, updateParams)
+          .transacting(trx);
+      })
+      .catch(err => {
+        return undefined;
+      })
       .then(result => {
         return undefined;
       });
