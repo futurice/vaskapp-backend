31ac10150e6c14f4d0fecefc394ea67c450c1662
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-01 08:38:38 +0200

    Feed zero time to Jan 1st, 2017
    Reduces the size of the hot score integers

--- src/constants.js
+++ src/constants.js
@@ -13,7 +13,8 @@ const CONST = {
     'new',
     'hot',
   ],
-  FEED_DECAY_INTERVAL: 60 * 60 * 2, // In 's' how often posts get +1 baseline hotness score.
+  FEED_INFLATION_INTERVAL: 60 * 60 * 2, // In 's' how often posts get +1 baseline hotness score.
+  FEED_ZERO_TIME: 1483228800, // January 1st, 2017. Time of 0 baseline hot score.
 };
 
 export default CONST;
--- src/core/vote-core.js
+++ src/core/vote-core.js
@@ -48,10 +48,11 @@ function createOrUpdateVote(vote) {
   });
 }
 
-function _calculateHotnessScore(votes, age) {
+function _calculateHotnessScore(votes, createdAt) {
   let order = Math.log10(Math.max(Math.abs(votes), 1));
   let sign = Math.sign(votes);
-  return Math.round(order + sign * age / CONST.FEED_DECAY_INTERVAL);
+  let age = createdAt - CONST.FEED_ZERO_TIME;
+  return Math.round(order + sign * age / CONST.FEED_INFLATION_INTERVAL);
 }
 
 export {
