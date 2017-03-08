773b5ae84d6ce55a4d527ef8fa848da3f7a7800e
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-08 08:41:24 +0200

    Capital letters for city names

--- seeds/teams.js
+++ seeds/teams.js
@@ -14,13 +14,13 @@ exports.seed = function(knex, Promise) {
   .then((result) => {
     result.rows.forEach(city => {
       cities[city.name] = city.id;
-    })
+    });
     return null;
   })
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 1,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'TiTe',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/tite.png',
     });
@@ -28,7 +28,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 2,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Skilta',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/skilta.png'
     });
@@ -36,7 +36,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 3,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Autek',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/autek.png'
     });
@@ -44,7 +44,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 4,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Bioner',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/bioner.png'
     });
@@ -52,7 +52,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 5,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Hiukkanen',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/hiukkanen.png'
     });
@@ -60,7 +60,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 6,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Indecs',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/indecs.png'
     });
@@ -68,7 +68,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 7,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'KoRK',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/kork.png'
     });
@@ -76,7 +76,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 8,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Man@ger',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/manager.png'
     });
@@ -84,7 +84,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 9,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'MIK',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/mik.png'
     });
@@ -92,7 +92,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 10,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'TamArk',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/tamark.png'
     });
@@ -100,7 +100,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 11,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'TARAKI',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/taraki.png'
     });
@@ -108,7 +108,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 12,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'YKI',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/yki.png'
     });
@@ -116,7 +116,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 13,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'TeLE',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/tele.png'
     });
@@ -124,7 +124,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 14,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'ESN INTO',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/esn.png'
     });
@@ -132,7 +132,7 @@ exports.seed = function(knex, Promise) {
   .then(() => {
     return util.insertOrUpdate(knex, 'teams', {
       id: 15,
-      city_id: cities['tampere'],
+      city_id: cities['Tampere'],
       name: 'Wapputiimi',
       image_path: 'https://storage.googleapis.com/wappuapp/assets/wapputiimi.png'
     });
