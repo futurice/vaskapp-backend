5a5a57346a4f2a4207282652149b3304a87b2139
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-10 10:52:27 +0200

    Added event tabel

---
+++ migrations/20170310092939_add_events_table.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('events', table => {
+    table.string('id').primary().index(); // Format cityName_indexInFile
+    table.string('name', 100);
+    table.string('location_name', 100);
+    table.timestamp('start_time');
+    table.timestamp('end_time');
+    table.string('description', 5000);
+    table.string('organizer', 100);
+    table.string('contact_details', 200);
+    table.boolean('teemu');
+    table.specificType('location', 'point').index('event_location', 'GIST');
+    table.string('cover_image', 200);
+    table.boolean('show').defaultTo(true);
+    table.integer('city_id').unsigned().index();
+    table.foreign('city_id')
+      .references('id')
+      .inTable('cities')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('events');
+};
