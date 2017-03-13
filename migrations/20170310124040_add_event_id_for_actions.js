45d8406eea3afee7049c26d5449e5b86e46eef61
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-13 08:43:52 +0200

    Comment to explain the magic number

--- migrations/20170310124040_add_event_id_for_actions.js
+++ migrations/20170310124040_add_event_id_for_actions.js
@@ -13,10 +13,11 @@ exports.up = function(knex, Promise) {
       only_one_check_in_per_event
     ON
       actions(user_id, event_id)
+    -- Magic number 9 is CHECK_IN_EVENT action type ID.
     WHERE action_type_id = 9;
   `);
 };
-// ALTER TABLE actions ADD CONSTRAINT actions_user_event_uniq UNIQUE (user_id, event_id)
+
 exports.down = function(knex, Promise) {
   return knex.schema.table('actions', function(table) {
     table.dropColumn('event_id');
