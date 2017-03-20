fd27c7c87be82c265256f8fa450fe00cad3f5391
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-20 09:43:23 +0200

    Column 'url' renamed to 'stream'

--- migrations/20170314082605_add_radio_table.js
+++ migrations/20170314082605_add_radio_table.js
@@ -3,8 +3,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('radios', function(table) {
     table.increments('id').primary().index();
     table.string('name').notNullable().index();
-    table.string('url');
     table.string('now_playing');
+    table.string('stream');
 
     table.integer('city_id').unsigned().notNullable().index();
     table.foreign('city_id')
