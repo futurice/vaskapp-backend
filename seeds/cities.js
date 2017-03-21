f2cd20c4a2f903668e44f34a3d6957859e610cc9
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-21 14:45:42 +0200

    Renamed 'Helsinki' to 'Otaniemi'. Removed 'Other'.

--- seeds/cities.js
+++ seeds/cities.js
@@ -4,14 +4,10 @@ var util = require('../src/util/seeds');
 exports.seed = function(knex, Promise) {
   return util.insertOrUpdate(knex, 'cities', {
     id: 1,
-    name: 'Other',
+    name: 'Otaniemi',
   })
   .then(() => util.insertOrUpdate(knex, 'cities', {
     id: 2,
-    name: 'Helsinki',
-  }))
-  .then(() => util.insertOrUpdate(knex, 'cities', {
-    id: 3,
     name: 'Tampere',
   }));
 }
