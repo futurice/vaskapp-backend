3f6b6b1db300809bef2540df1c6f05d53e2482af
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-22 09:40:46 +0200

    Remove city 'Other' using removeIfExists

--- seeds/cities.js
+++ seeds/cities.js
@@ -2,12 +2,16 @@ var util = require('../src/util/seeds');
 
 
 exports.seed = function(knex, Promise) {
-  return util.insertOrUpdate(knex, 'cities', {
+  return util.removeIfExists(knex, 'cities', {
     id: 1,
-    name: 'Otaniemi',
+    name: 'Other',
   })
   .then(() => util.insertOrUpdate(knex, 'cities', {
     id: 2,
+    name: 'Otaniemi',
+  }))
+  .then(() => util.insertOrUpdate(knex, 'cities', {
+    id: 3,
     name: 'Tampere',
   }));
 }
