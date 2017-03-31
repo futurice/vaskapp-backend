a77b5754d9c783d43ceb2bcfffbab03d48dbc9d9
Tomy Salminen <tomy.salminen@futurice.com>
2017-03-31 14:18:54 +0300

    Endpoint returned combined score from actions and votes

--- src/core/team-core.js
+++ src/core/team-core.js
@@ -4,26 +4,48 @@ import {deepChangeKeyCase} from '../util';
 
 function getTeams(opts) {
   const isBanned = opts.client && !!opts.client.isBanned;
-
-  let sqlString = `
-    SELECT teams.id, teams.name, teams.image_path,
-      SUM(COALESCE(action_types.value, 0)) AS score,
-      teams.city_id AS city,
-      tmps.value
-    FROM teams
-    LEFT JOIN actions ON teams.id = actions.team_id ${isBanned ? '' : 'AND NOT actions.is_banned'}
-    LEFT JOIN action_types ON actions.action_type_id = action_types.id
-    LEFT JOIN (
-      SELECT
+  const voteScoreSql = `
+    (SELECT
+      team_id,
+      SUM(value) AS value
+    FROM
+      (SELECT
         wilsons(
-          COUNT(CASE votes.value WHEN 1 THEN 1 ELSE null END)::int,
-          COUNT(CASE votes.value WHEN -1 THEN -1 ELSE null END)::int) * COUNT(votes)::numeric AS value,
-        users.team_id AS team
+            COUNT(CASE votes.value WHEN 1 THEN 1 ELSE null END)::int,
+            COUNT(CASE votes.value WHEN -1 THEN -1 ELSE null END)::int
+          ) * COUNT(votes)::numeric AS value,
+        users.team_id AS team_id
       FROM feed_items
       LEFT JOIN users ON users.id = feed_items.user_id
       LEFT JOIN votes ON votes.feed_item_id = feed_items.id
-      GROUP BY users.team_id
-    ) AS tmps ON tmps.team = teams.id
+      WHERE feed_items.user_id IS NOT NULL AND NOT users.is_banned
+      GROUP BY feed_items.id, users.team_id
+    ) AS sub_query
+    GROUP BY team_id)
+  `;
+
+  const actionScoreSql = `
+    (SELECT
+      SUM(COALESCE(action_types.value, 0)) AS value,
+      teams.id AS team_id,
+      teams.name AS team_name,
+      teams.image_path AS image_path,
+      teams.city_id AS city_id
+    FROM teams
+    LEFT JOIN actions ON teams.id = actions.team_id ${isBanned ? '' : 'AND NOT actions.is_banned'}
+    LEFT JOIN action_types ON actions.action_type_id = action_types.id
+    GROUP BY teams.id)
+  `;
+
+  let sqlString = `
+    SELECT
+      actions_score.team_id AS id,
+      actions_score.team_name AS name,
+      actions_score.image_path,
+      SUM(COALESCE(actions_score.value, 0)) + SUM(COALESCE(vote_score.value, 0)) as score,
+      actions_score.city_id AS city
+    FROM ${ actionScoreSql } AS actions_score
+    LEFT JOIN ${ voteScoreSql } vote_score ON vote_score.team_id = actions_score.team_id
   `;
 
   let params = [];
@@ -39,8 +61,8 @@ function getTeams(opts) {
   }
 
   sqlString += `
-    GROUP BY teams.id, teams.name, tmps.value
-    ORDER BY score DESC, teams.id
+    GROUP BY id, name, actions_score.image_path, city
+    ORDER BY score DESC, id
   `;
 
   return knex.raw(sqlString, params)
