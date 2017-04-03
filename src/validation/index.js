6c956e9ae414a0a604545b8458149da46437fdc5
Tomy Salminen <tomy.salminen@futurice.com>
2017-04-03 09:24:23 +0300

    assert() returns the validated object instead of the given parameter

--- src/validation/index.js
+++ src/validation/index.js
@@ -90,18 +90,18 @@ function assert(obj, schemaName) {
   var expected = {};
   expected[schemaName] = joiObj;
 
-  try {
-    Joi.assert(content, expected);
-  } catch (err) {
-    if (!err.isJoi) {
-      throw err;
+  var result = Joi.validate(content, expected);
+
+  if (result.error) {
+    if (!result.error.isJoi) {
+      throw result.error;
     }
 
-    _throwJoiError(err);
-  };
+    _throwJoiError(result.error);
+  }
 
   // Return for convenience
-  return obj;
+  return result.value[schemaName];
 }
 
 // Converts string `value` to a correct js object.
