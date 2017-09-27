9ceaf311037e89139e4a2b2b75e9b6452949c34c
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-09-27 17:37:04 +0300

    remove compose

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -3,15 +3,13 @@ const compose = require('composable-middleware');
 const validateJwt = expressJwt({ secret: process.env.AUTH0_SECRET_KEY });
 
 // # Validates JWT
-function isAuthenticated() {
-  return compose()
-  .use(function(req, res, next) {
-    // Allow access_token to be passed through query parameter as well
-    if (req.query && req.query.hasOwnProperty('access_token')) {
-      req.headers.authorization = 'Bearer ' + req.query.access_token;
-    }
-    validateJwt(req, res, next);
-  })
+function isAuthenticated(req, res, next)) {
+  // Allow access_token to be passed through query parameter as well
+  if (req.query && req.query.hasOwnProperty('access_token')) {
+    req.headers.authorization = 'Bearer ' + req.query.access_token;
+  }
+
+  return validateJwt(req, res, next);
 }
 
 exports.isAuthenticated = isAuthenticated;
