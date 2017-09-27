89aa5bc29462e6ce0ac0cafd04f2fc3a3022b94e
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-09-27 16:17:57 +0300

    Adds authentication to feed endpoint

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ import * as radioHttp from './http/radio-http';
 import * as wappuMood from './http/wappu-mood-http';
 import * as imageHttp from './http/image-http';
 
+import * as auth from './auth/auth.service';
 
 function createRouter() {
   const router = express.Router();
@@ -29,7 +30,7 @@ function createRouter() {
 
   router.get('/action_types', actionTypeHttp.getActionTypes);
 
-  router.get('/feed', feedHttp.getFeed);
+  router.get('/feed', auth.isisAuthenticated(), feedHttp.getFeed);
   router.delete('/feed/:id', feedHttp.deleteFeedItem);
   router.get('/feed/:id', feedHttp.getFeedItem);
 
