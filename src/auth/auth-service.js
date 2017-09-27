539e8cdea19bfbe82069c43aa3b4e21c184e73aa
Pasi Lampinen <plam@fl027.local>
2017-09-27 19:32:56 +0300

    change isAuth logic

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -3,13 +3,22 @@ const compose = require('composable-middleware');
 const validateJwt = expressJwt({ secret: process.env.AUTH0_SECRET_KEY });
 
 // # Validates JWT
-function isAuthenticated(req, res, next) {
-  // Allow access_token to be passed through query parameter as well
-  if (req.query && req.query.hasOwnProperty('access_token')) {
-    req.headers.authorization = 'Bearer ' + req.query.access_token;
-  }
-
-  validateJwt(req, res, next);
+function isAuthenticated() {
+  return compose()
+  .use(function(req, res, next) {
+    // Allow access_token to be passed through query parameter as well
+    if (req.query && req.query.hasOwnProperty('access_token')) {
+      req.headers.authorization = 'Bearer ' + req.query.access_token;
+    }
+    validateJwt(req, res, next);
+  })
+  // .use(function(err, req, res, next) {
+  //   if (err.name === 'UnauthorizedError') {
+  //     return res.status(401).send('invalid token...');
+  //   }
+  //   next()
+  // });
 }
+
 
 exports.isAuthenticated = isAuthenticated;
