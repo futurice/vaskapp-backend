e4707bec3f3f090e1180f4663a133001c30afcbd
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-09-27 17:37:35 +0300

    fix typo

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -3,7 +3,7 @@ const compose = require('composable-middleware');
 const validateJwt = expressJwt({ secret: process.env.AUTH0_SECRET_KEY });
 
 // # Validates JWT
-function isAuthenticated(req, res, next)) {
+function isAuthenticated(req, res, next) {
   // Allow access_token to be passed through query parameter as well
   if (req.query && req.query.hasOwnProperty('access_token')) {
     req.headers.authorization = 'Bearer ' + req.query.access_token;
