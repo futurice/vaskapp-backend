108ea629f4f97b8ff74b76e66e5c88616275cbed
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-09-27 16:17:41 +0300

    Adds authentication service

---
+++ src/auth/auth.service.js
@@ -0,0 +1,18 @@
+const expressJwt = require('express-jwt');
+const compose = require('composable-middleware');
+const validateJwt = expressJwt({ secret: process.env.AUTH0_SECRET_KEY });
+
+// # Validates JWT
+function isAuthenticated() {
+  return compose()
+  .use(function(req, res, next) {
+    // Allow access_token to be passed through query parameter as well
+    if(req.query && req.query.hasOwnProperty('access_token')) {
+      req.headers.authorization = 'Bearer ' + req.query.access_token;
+    }
+    validateJwt(req, res, next);
+  })
+}
+
+
+exports.isAuthenticated = isAuthenticated;
