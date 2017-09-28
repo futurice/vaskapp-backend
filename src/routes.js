235dc772245391dc6fb051254e567440848a5610
Pasi Lampinen <plam@fl027.local>
2017-09-28 10:02:42 +0300

    adds auth to routes

--- src/routes.js
+++ src/routes.js
@@ -17,31 +17,31 @@ import * as auth from './auth/auth-service';
 function createRouter() {
   const router = express.Router();
 
-  router.get('/events', eventHttp.getEvents);
-  router.get('/events/:id', eventHttp.getEvent);
+  router.get('/events', auth.isAuthenticated(), eventHttp.getEvents);
+  router.get('/events/:id', auth.isAuthenticated(), eventHttp.getEvent);
 
-  router.post('/actions', actionHttp.postAction);
-  router.get('/teams', teamHttp.getTeams);
+  router.post('/actions', auth.isAuthenticated(), actionHttp.postAction);
+  router.get('/teams', auth.isAuthenticated(), teamHttp.getTeams);
 
-  router.get('/users', userHttp.getUserById);
-  router.put('/users/:uuid', userHttp.putUser);
-  router.get('/users/:uuid', userHttp.getUserByUuid);
+  router.get('/users', auth.isAuthenticated(), userHttp.getUserById);
+  router.put('/users/:uuid', auth.isAuthenticated(), userHttp.putUser);
+  router.get('/users/:uuid', auth.isAuthenticated(), userHttp.getUserByUuid);
 
   router.get('/action_types', actionTypeHttp.getActionTypes);
 
   router.get('/feed', auth.isAuthenticated(), feedHttp.getFeed);
-  router.delete('/feed/:id', feedHttp.deleteFeedItem);
-  router.get('/feed/:id', feedHttp.getFeedItem);
+  router.delete('/feed/:id', auth.isAuthenticated(), feedHttp.deleteFeedItem);
+  router.get('/feed/:id', auth.isAuthenticated(), feedHttp.getFeedItem);
 
-  router.get('/image/:id', imageHttp.getImage);
+  router.get('/image/:id', auth.isAuthenticated(), imageHttp.getImage);
 
-  router.get('/announcements', announcementHttp.getAnnouncements);
+  router.get('/announcements', auth.isAuthenticated(), announcementHttp.getAnnouncements);
 
-  router.get('/markers', markerHttp.getMarkers);
+  router.get('/markers', auth.isAuthenticated(), markerHttp.getMarkers);
 
-  router.get('/cities', citiesHttp.getCities)
+  router.get('/cities', auth.isAuthenticated(), citiesHttp.getCities)
 
-  router.put('/vote', voteHttp.putVote);
+  router.put('/vote', auth.isAuthenticated(), voteHttp.putVote);
 
   router.get('/radio', radioHttp.getStations);
   router.get('/radio/:id', radioHttp.getStation);
