c409e025e9ffc8fc36aab6f23c7ec8598d18053a
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-09-29 13:06:42 +0300

    try auth0 /delegation

--- src/http/auth-http.js
+++ src/http/auth-http.js
@@ -4,18 +4,32 @@ import {createJsonRoute} from '../util/express';
 const refreshAuthToken = createJsonRoute(function(req, res) {
   const token = req.params.refreshToken;
 
+  // var options = {
+  //   method: 'POST',
+  //   url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+  //   headers: { 'content-type': 'application/json' },
+  //   body: {
+  //     refresh_token: token,
+  //     grant_type: 'refresh_token',
+  //     client_id: process.env.AUTH0_CLIENT_ID,
+  //     client_secret: process.env.AUTH0_SECRET_KEY,
+  //   },
+  //   json: true
+  // };
+
+
   var options = {
     method: 'POST',
-    url: `https://${process.env.AUTH0_DOMAIN}/oauth/token`,
+    url: `https://${process.env.AUTH0_DOMAIN}/delegation`,
     headers: { 'content-type': 'application/json' },
     body: {
-      refresh_token: token,
-      grant_type: 'refresh_token',
+      id_token: token,
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
       client_id: process.env.AUTH0_CLIENT_ID,
-      client_secret: process.env.AUTH0_SECRET_KEY,
     },
     json: true
   };
+
 
   return new Promise(function(resolve, reject) {
     request(options, function (error, response, body) {
