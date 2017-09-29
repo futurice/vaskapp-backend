d8073ea7eb559b255a0931da1a2a658b8faf435d
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-09-29 11:19:20 +0300

    back to basic jwt check in routes

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -4,31 +4,31 @@ const compose = require('composable-middleware');
 const validateJwt = jwt({ secret: process.env.AUTH0_SECRET_KEY });
 
 // # Validates JWT
-// function isAuthenticated() {
-//   return compose()
-//   .use(function(req, res, next) {
-//     // Allow access_token to be passed through query parameter as well
-//     if (req.query && req.query.hasOwnProperty('access_token')) {
-//       req.headers.authorization = 'Bearer ' + req.query.access_token;
-//     }
+function isAuthenticated() {
+  return compose()
+  .use(function(req, res, next) {
+    // Allow access_token to be passed through query parameter as well
+    if (req.query && req.query.hasOwnProperty('access_token')) {
+      req.headers.authorization = 'Bearer ' + req.query.access_token;
+    }
 
-//     validateJwt(req, res, next);
-//   });
-// }
+    validateJwt(req, res, next);
+  });
+}
 
-const jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
-  audience: 'https://praha-app-qa.herokuapp.com/api',
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
-});
+// const jwtCheck = jwt({
+//   secret: jwks.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+//   }),
+//   audience: 'https://praha-app-qa.herokuapp.com/api',
+//   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+//   algorithms: ['RS256']
+// });
 
-const isAuthenticated = jwtCheck;
+// const isAuthenticated = jwtCheck;
 
 export {
   isAuthenticated
--- src/routes.js
+++ src/routes.js
@@ -12,45 +12,45 @@ import * as citiesHttp from './http/cities-http';
 import * as radioHttp from './http/radio-http';
 import * as wappuMood from './http/wappu-mood-http';
 import * as imageHttp from './http/image-http';
-import * as authHttp from './http/auth-http';
 import * as authService from './auth/auth-service';
+// import * as authHttp from './http/auth-http';
 
 function createRouter() {
   const router = express.Router();
 
-  router.get('/events', eventHttp.getEvents);
-  router.get('/events/:id', eventHttp.getEvent);
+  router.get('/events', authService.isAuthenticated(), eventHttp.getEvents);
+  router.get('/events/:id', authService.isAuthenticated(), eventHttp.getEvent);
 
-  router.post('/actions', actionHttp.postAction);
-  router.get('/teams', teamHttp.getTeams);
+  router.post('/actions', authService.isAuthenticated(), actionHttp.postAction);
+  router.get('/teams', authService.isAuthenticated(), teamHttp.getTeams);
 
-  router.get('/users', userHttp.getUserById);
-  router.put('/users/:uuid', userHttp.putUser);
-  router.get('/users/:uuid', userHttp.getUserByUuid);
+  router.get('/users', authService.isAuthenticated(), userHttp.getUserById);
+  router.put('/users/:uuid', authService.isAuthenticated(), userHttp.putUser);
+  router.get('/users/:uuid', authService.isAuthenticated(), userHttp.getUserByUuid);
 
   // router.post('/auth/:refreshToken', authHttp.refreshAuthToken); // no auth, because token should be expired
 
-  router.get('/action_types', actionTypeHttp.getActionTypes);
+  router.get('/action_types', authService.isAuthenticated(), actionTypeHttp.getActionTypes);
 
-  router.get('/feed', feedHttp.getFeed);
-  router.delete('/feed/:id', feedHttp.deleteFeedItem);
-  router.get('/feed/:id', feedHttp.getFeedItem);
+  router.get('/feed', authService.isAuthenticated(), feedHttp.getFeed);
+  router.delete('/feed/:id', authService.isAuthenticated(), feedHttp.deleteFeedItem);
+  router.get('/feed/:id', authService.isAuthenticated(), feedHttp.getFeedItem);
 
-  router.get('/image/:id', imageHttp.getImage);
+  router.get('/image/:id', authService.isAuthenticated(), imageHttp.getImage);
 
-  router.get('/announcements', announcementHttp.getAnnouncements);
+  router.get('/announcements', authService.isAuthenticated(), announcementHttp.getAnnouncements);
 
-  router.get('/markers', markerHttp.getMarkers);
+  router.get('/markers', authService.isAuthenticated(), markerHttp.getMarkers);
 
-  router.get('/cities', citiesHttp.getCities)
+  router.get('/cities', authService.isAuthenticated(), citiesHttp.getCities)
 
-  router.put('/vote', voteHttp.putVote);
+  router.put('/vote', authService.isAuthenticated(), voteHttp.putVote);
 
-  router.get('/radio', radioHttp.getStations);
-  router.get('/radio/:id', radioHttp.getStation);
+  router.get('/radio', authService.isAuthenticated(), radioHttp.getStations);
+  router.get('/radio/:id', authService.isAuthenticated(), radioHttp.getStation);
 
-  router.put('/mood', wappuMood.putMood);
-  router.get('/mood', wappuMood.getMood);
+  router.put('/mood', authService.isAuthenticated(), wappuMood.putMood);
+  router.get('/mood', authService.isAuthenticated(), wappuMood.getMood);
 
   return router;
 }
