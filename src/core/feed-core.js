707bc68b65a985d077893b0304b5cd453141a3bc
Pasi Lampinen <plam@fl027.local>
2017-10-07 02:23:47 +0300

    inject params to sql

--- src/core/feed-core.js
+++ src/core/feed-core.js
@@ -359,10 +359,10 @@ function _getWhereSql(opts) {
 
   if (opts.longitude && opts.latitude && opts.radius) {
 
-    whereClauses.push(`ST_DWithin(Geometry(location), ST_GeogFromText('POINT(? ?)'), ?)`);
-    params.push(opts.longitude);
-    params.push(opts.latitude);
-    params.push(opts.radius);
+    whereClauses.push(`ST_DWithin(Geometry(feed_items.location), ST_GeogFromText('POINT(${opts.longitude} ${opts.latitude})'), ${opts.radius})`);
+    // params.push(opts.longitude);
+    // params.push(opts.latitude);
+    // params.push(opts.radius);
   }
 
   return whereClauses.length > 0
