caff41bfa2a4fb11b37e68b591574503ce24e570
Pasi Lampinen <plam@fl027.local>
2017-11-04 11:59:28 +0200

    Add DISABLE_AUTH option to jwt check

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -6,6 +6,12 @@ const validateJwt = jwt({ secret: process.env.AUTH0_SECRET_KEY });
 function isAuthenticated() {
   return compose()
   .use(function(req, res, next) {
+
+    // Do not require token in development or test env
+    if (process.env.DISABLE_AUTH === 'true') {
+      return next();
+    }
+
     // Allow access_token to be passed through query parameter as well
     if (req.query && req.query.hasOwnProperty('access_token')) {
       req.headers.authorization = 'Bearer ' + req.query.access_token;
