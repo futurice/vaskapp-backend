930a2e913ab9a7156a57f864de3f0e6cc4646b6c
Pasi Lampinen <plam@fl027.local>
2017-11-11 12:56:26 +0200

    Add city_id column for markers

---
+++ migrations/20171111100102_add_marker_city_id_column.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('markers', function(table) {
+    table.integer('city_id').unsigned().index();
+    table.foreign('city_id')
+      .references('id')
+      .inTable('cities')
+      .onDelete('RESTRICT')
+      .onUpdate('CASCADE');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('markers', function(table) {
+    table.dropColumn('city_id');
+  });
+};
