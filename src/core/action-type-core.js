a46310fe52bab9f082e7af190913544ddbac4b20
Pasi Lampinen <plam@fl027.local>
2017-11-11 03:22:17 +0200

    test is user part of req

--- src/core/action-type-core.js
+++ src/core/action-type-core.js
@@ -1,17 +1,20 @@
 const {knex} = require('../util/database').connect();
 import _ from 'lodash';
 
-function getActionTypes() {
+function getActionTypes(user) {
   return knex('action_types')
     .select('*')
     .where('is_user_action', true)
     .then(rows => {
-      return _.map(rows, _actionTypeRowToObject);
+      return _.map(rows, function(row) {
+        return _actionTypeRowToObject(row, user);
+      });
     });
 }
 
-function _actionTypeRowToObject(row) {
+function _actionTypeRowToObject(row, user) {
   return {
+    user: user,
     id: row.id,
     code: row.code,
     name: row.name,
--- src/http/action-type-http.js
+++ src/http/action-type-http.js
@@ -2,7 +2,7 @@ import * as actionTypeCore from '../core/action-type-core';
 import {createJsonRoute} from '../util/express';
 
 let getActionTypes = createJsonRoute(function(req, res) {
-  return actionTypeCore.getActionTypes();
+  return actionTypeCore.getActionTypes(req.user);
 });
 
 export {
