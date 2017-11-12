5ff5c6bc76b0a3c265d1057ba861a4432c85b964
Pasi Lampinen <plam@fl027.local>
2017-11-12 18:07:13 +0200

    Add city from domain to request

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import * as citiesCore from '../core/cities-core';
+
 const jwt = require('express-jwt');
 const compose = require('composable-middleware');
 const validateJwt = jwt({ secret: process.env.AUTH0_SECRET_KEY });
@@ -18,6 +21,32 @@ function isAuthenticated() {
     }
 
     validateJwt(req, res, next);
+  })
+  .use(function(req, res, next) {
+
+    // Parse domain from email address
+    const email = _.get(req, ['user', 'email'], '');
+    const domain = email.substring(email.lastIndexOf('@') + 1);
+
+    if (_.isEmpty(domain)) {
+      next();
+    }
+
+    citiesCore.findByDomain(domain)
+      .then(city => {
+
+        // Create city & team for domain
+        if (!city) {
+          citiesCore.createCity(domain)
+          .then(newCity => {
+            req.city = newCity;
+            return next();
+          })
+        }
+
+        req.city = city;
+        next();
+      });
   });
 }
 
