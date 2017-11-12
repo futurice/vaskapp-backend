6ee11969bd4ac727f111c370ec222fb0d6607aeb
Pasi Lampinen <plam@fl027.local>
2017-11-12 10:33:04 +0200

    update city domain

--- migrations/20171112102802_add_city_domain_column.js
+++ migrations/20171112102802_add_city_domain_column.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.table('cities', function(table) {
-    table.string('domain').notNullable().unique().index();
+    table.string('domain').unique();
   });
 };
 
