dffe1827beebfee50c5545cac5c7ee0e15eb03e4
Pasi Lampinen <plam@fl027.local>
2017-11-13 22:49:08 +0200

    update init database sql

--- tools/init-database.sql
+++ tools/init-database.sql
@@ -1,5 +1,5 @@
-CREATE USER praha WITH PASSWORD 'praha';
-CREATE DATABASE praha_app lc_collate 'fi_FI.UTF-8' lc_ctype 'fi_FI.UTF-8' encoding 'UTF8' template template0;
-GRANT ALL PRIVILEGES ON DATABASE praha_app to praha;
-CREATE DATABASE praha_app_test lc_collate 'fi_FI.UTF-8' lc_ctype 'fi_FI.UTF-8' encoding 'UTF8' template template0;
-GRANT ALL PRIVILEGES ON DATABASE praha_app_test to praha;
+CREATE USER vask WITH PASSWORD 'vask';
+CREATE DATABASE vask_app lc_collate 'fi_FI.UTF-8' lc_ctype 'fi_FI.UTF-8' encoding 'UTF8' template template0;
+GRANT ALL PRIVILEGES ON DATABASE vask_app to vask;
+CREATE DATABASE vask_app_test lc_collate 'fi_FI.UTF-8' lc_ctype 'fi_FI.UTF-8' encoding 'UTF8' template template0;
+GRANT ALL PRIVILEGES ON DATABASE vask_app_test to vask;
